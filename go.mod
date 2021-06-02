5c3b396b14c71a660883d139fee6c9fd6240bec5
Artyom Pervukhin <github@artyom.dev>
2021-06-02 16:18:20 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/artyom/cfn-deploy
+
+go 1.16
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.6.0
+	github.com/aws/aws-sdk-go-v2/config v1.3.0
+	github.com/aws/aws-sdk-go-v2/service/cloudformation v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,64 @@
+// cfn-deploy creates or updates CloudFormation stack from a template file
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
+)
+
+func main() {
+	log.SetFlags(0)
+	args := runArgs{}
+	flag.StringVar(&args.File, "f", args.File, "template `file`")
+	flag.StringVar(&args.Name, "n", args.Name, "stack `name`")
+	flag.BoolVar(&args.Create, "c", args.Create, "create stack instead of updating")
+	flag.Parse()
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	if err := run(ctx, args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type runArgs struct {
+	File   string
+	Name   string
+	Create bool
+}
+
+func run(ctx context.Context, args runArgs) error {
+	body, err := os.ReadFile(args.File)
+	if err != nil {
+		return err
+	}
+	if args.Name == "" {
+		return errors.New("stack name must be set")
+	}
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return err
+	}
+	client := cloudformation.NewFromConfig(cfg)
+	if args.Create {
+		_, err = client.CreateStack(ctx, &cloudformation.CreateStackInput{
+			StackName:    &args.Name,
+			TemplateBody: aws.String(string(body)),
+			OnFailure:    types.OnFailureDelete,
+		})
+		return err
+	}
+	_, err = client.UpdateStack(ctx, &cloudformation.UpdateStackInput{
+		StackName:    &args.Name,
+		TemplateBody: aws.String(string(body)),
+	})
+	return err
+}
